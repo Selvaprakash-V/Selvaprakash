dceb5ff79784b6880c73fa69c037ccc935704a7d
Selvaprakash-V <selvaprakashv007@gmail.com>
2025-12-03 11:09:55 +0530

    added loction on map

--- frontend/client/src/components/LocationMap/LocationMap.js
+++ frontend/client/src/components/LocationMap/LocationMap.js
@@ -6,8 +6,8 @@ const LocationMap = () => {
   const location = {
     lat: 10.826032,
     lng: 77.060364,
-    name: 'Kondampatty, Tamil Nadu',
-    country: 'India',
+    name: 'India',
+    country: 'coimbatore',
     plusCode: 'R3F6+XG7'
   };
 
@@ -18,12 +18,15 @@ const LocationMap = () => {
         <div className="world-map-wrapper">
           <iframe
             className="world-map-frame"
-            src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d25000000!2d78.0!3d20.0!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1234567890!5m2!1sen!2sin`}
-            allowFullScreen=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d290.8492627265851!2d77.06051617829496!3d10.82576209951566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba84ee37569ae7f%3A0x3c5b1824b6e79192!2sSri%20Eshwar%20College%20of%20Engineering%2C%20Coimbatore!5e1!3m2!1sen!2sin!4v1764734877243!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-            title="World Location Map"
-          ></iframe>
+            title="Sri Eshwar College - India"
+          />
           
           {/* Profile Avatar Marker */}
           <div className="avatar-marker">
@@ -31,12 +34,6 @@ const LocationMap = () => {
             <div className="avatar-circle">
               <span className="avatar-emoji">👨‍💻</span>
             </div>
-          </div>
-
-          {/* Available Badge */}
-          <div className="availability-badge">
-            <span className="status-dot">🌐</span>
-            <span>Available Internationally</span>
           </div>
         </div>
 
